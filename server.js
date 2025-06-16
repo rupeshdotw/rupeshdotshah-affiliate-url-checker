d62e309998c6a760e1565b420d82eb4761e4cb3b
rupeshdotw <rupesh.w@3dot14.co>
2025-06-16 10:18:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 9000;
+const PORT = 8080;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
